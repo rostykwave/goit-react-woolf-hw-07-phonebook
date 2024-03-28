35e51c45d260103200880bf302a614b66e3d0e5a
Rostyslav Kovalchuk <rostykwave@gmail.com>
2024-03-28 16:45:34 +0200

    remove redux persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,6 @@
-import persistStore from 'redux-persist/es/persistStore'
-import { reducer } from './reducer'
-import { configureStore } from '@reduxjs/toolkit'
-import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
+import { reducer } from './reducer';
+import { configureStore } from '@reduxjs/toolkit';
 
 export const store = configureStore({
-	reducer,
-	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware({
-			serializableCheck: {
-				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-			},
-		}),
-})
-export const persistor = persistStore(store)
+  reducer,
+});
